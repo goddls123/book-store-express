9a54b3067719cfbddffc4c62e561c526db1e5b77
Jun <coolch0916@gmail.com>
2024-01-11 13:33:12 +0900

    feat : add user module validater

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const app = express();
 const userRouter = require("./routes/users.js");
 
@@ -10,6 +9,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send("book store");
 });
-app.use("/user", userRouter);
+app.use("/", userRouter);
 
 app.listen(process.env.PORT);
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,98 @@
 const express = require("express");
 
 const router = express.Router();
+const { validationResult, body } = require("express-validator");
 
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+  if (!err.isEmpty) {
+    return res.status(400).json(err.array());
+  }
+  next();
+};
 //로그인
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (email && password) {
-    if (email) {
+router.post(
+  "/login",
+  [
+    body("email")
+      .notEmpty()
+      .isEmail()
+      .withMessage("올바른 email와 password를 입력하세요"),
+    body("password")
+      .notEmpty()
+      .isString()
+      .withMessage("올바른 email와 password를 입력하세요"),
+    validate,
+  ],
+  (req, res) => {
+    const { email, password } = req.body;
+    if (password) {
       res.status(201).send("로그인 성공");
     } else {
-      res.status(404).json({ message: "다른 비밀 번호 입니다" });
+      res.status(403).json({ message: "올바른 email와 password를 입력하세요" });
     }
-  } else {
-    res.status(404).json({ message: "올바른 email와 password를 입력하세요" });
   }
-});
+);
 
 //회원가입
-router.post("/join", (req, res) => {
-  const { email, password } = req.body;
-  if (email && password) {
+router.post(
+  "/join",
+  [
+    body("email")
+      .notEmpty()
+      .isEmail()
+      .withMessage("올바른 email와 password를 입력하세요"),
+    body("password")
+      .notEmpty()
+      .isString()
+      .withMessage("올바른 email와 password를 입력하세요"),
+    validate,
+  ],
+  (req, res) => {
+    const { email, password } = req.body;
+
     if (email) {
       res.status(201).send("가입 성공");
     } else {
-      res.status(404).json({ message: "중복된 아이디가 존재 합니다." });
+      res.status(400).json({ message: "중복된 아이디가 존재 합니다." });
     }
-  } else {
-    res.status(404).json({ message: "올바른 email와 password를 입력하세요" });
   }
-});
+);
 
 //비밀번호 변경 요청
 router
   .route("/reset")
-  .post((req, res) => {
-    const { email } = req.body;
-    if (email) {
-      res.status(201).json(true);
-    } else {
-      res.status(404).json({ message: "없는 아이디 입니다." });
+  .post(
+    [
+      body("email")
+        .notEmpty()
+        .isEmail()
+        .withMessage("올바른 email와 password를 입력하세요"),
+      validate,
+    ],
+    (req, res) => {
+      const { email } = req.body;
+      if (email) {
+        res.status(201).json(true);
+      } else {
+        res.status(400).json({ message: "없는 아이디 입니다." });
+      }
     }
-  })
-  .put((req, res) => {
-    const { email, password } = req.body;
-    if (password) {
-      res.status(201).json(true);
-    } else {
-      res.status(404).json({ message: "올바르지 않는 password 입니다." });
+  )
+  .put(
+    [
+      body("password")
+        .notEmpty()
+        .isString()
+        .withMessage("올바른 email와 password를 입력하세요"),
+      validate,
+    ],
+    (req, res) => {
+      const { email, password } = req.body;
+
+      if (password) {
+        res.status(201).json(true);
+      }
     }
-  });
+  );
 module.exports = router;
