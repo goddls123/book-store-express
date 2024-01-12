791906a49468dea68486a77b16ff21366e75623a
Jun <coolch0916@gmail.com>
2024-01-12 09:05:16 +0900

    feat : Add jwt token and login

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,8 @@
 const conn = require("../mariadb.js");
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const join = (req, res) => {
   const { email, password } = req.body;
@@ -21,10 +24,31 @@ const login = (req, res) => {
   const sql = "SELECT * from users where email = ?";
 
   conn.query(sql, email, (err, results) => {
-    if (results[0] && results[0].password === password) {
-      res.status(201).send("로그인 성공");
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    const loginUser = results[0];
+
+    if (loginUser && loginUser.password === password) {
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: "5m",
+          issuer: "test",
+        }
+      );
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+      console.log(token);
+
+      return res.status(StatusCodes.OK).json(results);
     } else {
-      res.status(403).json({ message: "올바른 email와 password를 입력하세요" });
+      return res.status(StatusCodes.UNAUTHORIZED).end();
     }
   });
 };
--- routes/users.js
+++ routes/users.js
@@ -73,4 +73,5 @@ router
     ],
     passwordReset
   );
+
 module.exports = router;
